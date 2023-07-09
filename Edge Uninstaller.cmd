7038945e74caa19b83f32d08f5744ce4ba78d5ef
tiptop <105000222+tiptoppp@users.noreply.github.com>
2023-07-09 20:39:47 +0200

    Add files via upload

---
+++ Edge Uninstaller.cmd
@@ -0,0 +1,8 @@
+@echo off
+title Edge Uninstaller
+takeown /F "C:\Program Files (x86)\Microsoft" /A /R
+rmdir /s /Q "C:\Program Files (x86)\Microsoft\Edge"
+rmdir /s /Q "C:\Program Files (x86)\Microsoft\EdgeCore"
+rmdir /s /Q "C:\Program Files (x86)\Microsoft\EdgeUpdate"
+rmdir /s /Q "C:\Program Files (x86)\Microsoft\EdgeWebView"
+rmdir /s /Q "C:\Program Files (x86)\Microsoft\Temp"
